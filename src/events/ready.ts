832c7b321f0593dcd2c53604c02489644eb1522a
andrewchokh <andrewchokh@gmail.com>
2024-04-12 15:50:03 +0300

    Add MongoDB connection

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,26 @@
 import {Events} from "discord.js";
 import {Bot} from "../bot";
+import mongoose from "mongoose";
+import logger from "../logger";
+import {config} from "../config";
+
+async function connectToMongoDB(uri: string) {
+    try {
+        await mongoose.connect(uri);
+
+        logger.info('Successfully connected to MongoDB.')
+    }
+    catch (error) {
+        logger.error(error)
+    }
+}
 
 export const data: EventData = {
     name: Events.ClientReady,
     isOnce: true,
     callback: async (client: Bot) => {
-        console.log(`Logged in as ${client.user?.tag}`);
+        logger.info(`Logged in as ${client.user?.tag}`);
+
+        await connectToMongoDB(config.mongoUri as string);
     }
 }
