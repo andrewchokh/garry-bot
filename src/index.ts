e7d4b0603aef1f17b90cc0df738273b64ecfa0d5
andrewchokh <andrewchokh@gmail.com>
2024-04-15 15:35:18 +0300

    Optimize imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {Bot} from "./bot";
-import * as botOptions from "./config/bot.json"
+import botOptions from "./config/bot.json"
 import {ClientOptions} from "discord.js";
 
 const client = new Bot(botOptions as ClientOptions);
