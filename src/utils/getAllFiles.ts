b6e951a0d1b8b4233507a21a4f769297120db980
andrewchokh <andrewchokh@gmail.com>
2024-04-18 01:01:27 +0300

    Ignore index.ts/js

--- src/utils/getAllFiles.ts
+++ src/utils/getAllFiles.ts
@@ -9,7 +9,7 @@ export function getAllFiles(directory: string, foldersOnly: boolean = false) {
     for (const file of files) {
         const filePath = path.join(directory, file.name);
 
-        if (filePath.endsWith('index.ts')) continue;
+        if (filePath.endsWith('index.ts') || filePath.endsWith('index.js')) continue;
 
         if (foldersOnly && file.isDirectory()) fileNames.push(filePath);
         if (!foldersOnly && file.isFile()) fileNames.push(filePath);
