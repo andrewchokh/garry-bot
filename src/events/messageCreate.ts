2e6e838c7691c34de821a684fb3dc76b3afc06fa
andrewchokh <andrewchokh@gmail.com>
2024-04-18 00:46:27 +0300

    Add level roles

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,7 +5,7 @@ export const data: EventData = {
     name: Events.MessageCreate,
     isOnce: false,
     callback: async (message: Message) => {
-        if (message.member?.user.bot) return;
+        if (message.member?.user.bot || !message.guild) return;
 
         await giveXp(message.member as GuildMember, 5);
     }
