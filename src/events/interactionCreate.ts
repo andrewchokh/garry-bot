11cbf3bf69f79f6ac6cdde2fd812e0ccb05b497a
andrewchokh <andrewchokh@gmail.com>
2024-04-18 00:47:19 +0300

    Add bot channel filter

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,11 +1,17 @@
 import {CommandInteraction, Events} from "discord.js";
 import logger from "../logger";
+import {fetchOrCreateGuild} from "../database/queries/guild";
 
 export const data: EventData = {
     name: Events.InteractionCreate,
     isOnce: false,
     callback: async (interaction: CommandInteraction) => {
         if (!interaction.isCommand()) return;
+
+        const guildRecord = await fetchOrCreateGuild(interaction.guildId as string);
+        const botChannelIds = guildRecord.preferences.botChannelIds;
+
+        if (botChannelIds.length && !botChannelIds.includes(interaction.guildId)) return;
 
         const { commandName } = interaction;
 
