80e0a25a8afd3c3cf09972e1fcbfbbfb23e071ae
andrewchokh <andrewchokh@gmail.com>
2024-04-24 13:27:20 +0300

    Change commands export

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -1,7 +1,9 @@
 import { Collection } from "discord.js";
 
 declare module "discord.js" {
-    export interface Client {
+    interface Client {
         commands: Collection<any, any>[];
     }
 }
+
+export {}
