645461a1666b6929c259f8dac2dcc66ed59d5112
andrewchokh <andrewchokh@gmail.com>
2024-04-24 13:25:23 +0300

    Change syntax of /avatar command

--- src/commands/general/avatar.ts
+++ src/commands/general/avatar.ts
@@ -11,7 +11,7 @@ const slashCommand = new SlashCommandBuilder()
 .setDescription('Sends user avatar.')
 .addUserOption(new SlashCommandUserOption()
     .setName("user")
-    .setRequired(true)
+    .setRequired(false)
     .setDescription("User of the server.")
 );
 
@@ -19,17 +19,17 @@ export const data: CommandData = {
     slashCommand: slashCommand,
 
     callback: async (interaction: CommandInteraction) => {
-        const user = interaction.options.getUser('user') as User;
+        const user = interaction.options.getUser('user') as User || interaction.user;
 
         if (!user?.avatarURL()) return await interaction.reply({
             content: 'The user has no avatar.',
             ephemeral: true,
-        })
+        });
 
         const embed = new EmbedBuilder()
         .setColor(0x0077C46B)
         .setAuthor({name: `${user?.displayName}'s avatar`})
-        .setImage(user.avatarURL(), );
+        .setImage(user.avatarURL());
 
         await interaction.reply({embeds: [embed]});
     }
